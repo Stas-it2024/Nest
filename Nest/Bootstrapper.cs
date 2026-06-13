f810500e7a16e048c1f4dd974a621bc4bf3659b9
Stas-it2024 <Stanislav.Kharchenko@edu.bib.de>
2026-06-13 18:37:27 +0200

    Projektdateien hinzufügen.

---
+++ Nest/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using Nest.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Nest
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<ShellViewModel>();
+        }
+    }
+}
